fa2a2527e5fc254face68743806780258d1488ad
Abhi Patidar <ababhi9644@gmail.com>
2024-06-05 20:49:11 +0530

    updating file

--- src/Hearthealthpredictor/pipelines/training_pipeline.py
+++ src/Hearthealthpredictor/pipelines/training_pipeline.py
@@ -1,12 +1,14 @@
-from src.Hearthealthpredictor.components.data_ingestion import DataIngestion
-from src.Hearthealthpredictor.components.data_transformation import DataTransformation
-from src.Hearthealthpredictor.components.model_trainer import ModelTrainer
-#from src.Hearthealthpredictor.components.model_evaluation import ModelEvaluation
 import os
 import sys
 from src.Hearthealthpredictor.logger import logging
 from src.Hearthealthpredictor.exception import customexception
 import pandas as pd
+
+from src.Hearthealthpredictor.components.data_ingestion import DataIngestion
+from src.Hearthealthpredictor.components.data_transformation import DataTransformation
+from src.Hearthealthpredictor.components.model_trainer import ModelTrainer
+from src.Hearthealthpredictor.components.model_evaluation import ModelEvaluation
+
 
 obj=DataIngestion()
 
@@ -16,37 +18,9 @@ data_transformation=DataTransformation()
 
 train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
 
+
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initate_model_training(train_arr,test_arr)
 
-'''class TrainingPipeline:
-    def start_data_ingestion(self):
-        try:
-            data_ingestion = DataIngestion()
-            train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
-            return train_data_path, test_data_path
-        except Exception as e:
-            raise customexception(e, sys)
-        
-    def start_data_transformation(self, train_data_path, test_data_path):
-        try:
-            data_transformation = DataTransformation()
-            train_arr, test_arr = data_transformation.initialize_data_transformation(train_data_path, test_data_path)
-            return train_arr, test_arr
-        except Exception as e:
-            raise customexception(e, sys)
-    
-    def start_model_training(self, train_arr, test_arr):
-        try:
-            model_trainer = ModelTrainer()
-            model_trainer.initiate_model_training(train_arr, test_arr)
-        except Exception as e:
-            raise customexception(e, sys)
-                
-    def start_training(self):
-        try:
-            train_data_path, test_data_path = self.start_data_ingestion()
-            train_arr, test_arr = self.start_data_transformation(train_data_path, test_data_path)
-            self.start_model_training(train_arr, test_arr)
-        except Exception as e:
-            raise customexception(e, sys)'''
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
